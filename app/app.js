c374c701368d187d220d8dc796fa07295d6e25b6
irfan87 <nerve2009@yahoo.com>
2016-08-07 06:10:53 +0800

    add controller

--- app/app.js
+++ app/app.js
@@ -2,11 +2,25 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [])
-.controller('MyController', function($scope){
+.run(function($rootScope){
+	$rootScope.name = "World";
+})
+.controller('ParentsController', function($scope){
+	$scope.person = {
+		greeted: false
+	};
+})
+.controller('ChildController', function($scope){
+	$scope.sayHello = function(){
+		$scope.person.name = "Irfan";
+		$scope.greeted = true;
+	};
+})
+.controller('ShowDateController', function($scope){
 	$scope.clock = {
 		now: new Date()
 	};
-	
+
 	var updateClock = function(){
 		$scope.clock.now = new Date();
 	};
@@ -16,4 +30,14 @@ angular.module('myApp', [])
 	}, 1000);
 
 	updateClock();
+})
+.controller('FirstController', function($scope){
+	$scope.counter = 0;
+	$scope.add = function(amount){
+		$scope.counter += amount;
+	};
+
+	$scope.substract = function(amount){
+		$scope.counter -= amount;
+	};
 })
